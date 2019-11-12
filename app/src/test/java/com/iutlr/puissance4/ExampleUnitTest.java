b355b3eebeda10a1278230f0d1a5be69055174ae
Lenn Angel <lenn.angel@serli.com>
2019-11-12 14:08:06 +0100

    Tests

--- app/src/test/java/com/iutlr/puissance4/ExampleUnitTest.java
+++ app/src/test/java/com/iutlr/puissance4/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.iutlr.puissance4;
 
+import com.iutlr.puissance4.exceptions.JoueurException;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,28 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test(expected = JoueurException.class)
+    public void notEnoughPlayersTest() throws Exception {
+        List<Joueur> joueurs = new ArrayList<>();
+        Plateau p = new Plateau(10, 10, joueurs);
+    }
+
+
+    @Test
+    public void gameVictoryTest() throws Exception {
+        Joueur j1 = new Joueur("J1", 0);
+        Joueur j2 = new Joueur("J2", 0);
+
+        List<Joueur> joueurs = new ArrayList<>();
+        joueurs.add(j1);
+        joueurs.add(j2);
+
+        Plateau p = new Plateau(10, 10, joueurs);
+
+        // ..
+
+        assertEquals(j1, p.getGagnant());
     }
 }
